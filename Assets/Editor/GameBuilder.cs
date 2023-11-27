35c91a8e00de88300dea40e6a8f6ff4af48dec23
Zengard <maskanda54@yandex.ru>
2023-11-27 21:58:28 +0300

    Update

--- Assets/Editor/GameBuilder.cs
+++ Assets/Editor/GameBuilder.cs
@@ -31,4 +31,31 @@ public class GameBuilder : MonoBehaviour
 
 
     }
+
+
+    public static void PerformWondowsBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/NetcodePlus/Demo/Scenes/Menu.unity", "Assets/NetcodePlus/Demo/Scenes/MenuLobby.unity", "Assets/NetcodePlus/Demo/Scenes/ServerGame.unity",
+        "Assets/NetcodePlus/Demo/Scenes/ServerLobby.unity", "Assets/NetcodePlus/Demo/Scenes/DemoPuzzle.unity", "Assets/NetcodePlus/Demo/Scenes/DemoTank.unity", "Assets/NetcodePlus/Demo/Scenes/DemoSimple.unity"};
+        buildPlayerOptions.locationPathName = "build/Android/Test.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build faild");
+        }
+
+
+    }
+
 }
